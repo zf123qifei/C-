69a5a5ca715c14d904b38a58c700b6fb5b45d117
zf123qifei <zhengfei20030106@outlook.com>
2022-09-17 17:26:19 +0800

    yiyiyi

---
+++ MFCApplicationmyfirstporjiect/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCApplicationmyfirstporjiect.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATIONMYFIRSTPORJIECT_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDR_MENU2                       131
+#define IDC_EDIT1                       1003
+#define IDC_CHECK1                      1004
+#define IDC_DATETIMEPICKER1             1005
+#define IDC_RADIO2                      1007
+#define IDC_RADIO3                      1008
+#define IDC_RADIO4                      1009
+#define IDC_RADIO5                      1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
